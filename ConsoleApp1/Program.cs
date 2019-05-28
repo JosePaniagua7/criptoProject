b493f680720a75eb0af4d1c1d1674033eecebf55
jose.paniagua <jose.paniagua@dev.uneatlantico>
2019-05-28 12:38:17 +0200

    RSA is working, XML writter is ready to implement, now trying to read and parse XML

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Linq;
 using System.Text;
+using System.Xml;
 using TDESEncrypter;
 
 namespace ConsoleApp1
@@ -25,6 +26,7 @@ namespace ConsoleApp1
             decryptedText= myEncrypter.decrypt(encryptedText);
             Console.WriteLine("Ahora decrypted is: " + decryptedText);*/
 
+
             /*String text = "Que ondas";
             byte[] textAsByteArray = new ASCIIEncoding().GetBytes(text);
 
@@ -40,6 +42,44 @@ namespace ConsoleApp1
             byte[] decryptedText = myRsa.Decrypt(encryptedText);
             String finalString= new ASCIIEncoding().GetString(decryptedText);
             Console.WriteLine("Fin desencriptacion: "+ finalString.Length);*/
+
+
+
+            //------------------------Here Starts XML writer------------------------------------------------
+            // Create a new file in C:\\ dir  
+            XmlTextWriter textWriter = new XmlTextWriter("C:\\Users\\jose.paniagua\\Desktop\\myFile.xml", null);
+            // Opens the document  
+            textWriter.WriteStartDocument();
+            // Write comments  
+            textWriter.WriteComment("Tengo Miedo!");
+            textWriter.WriteComment("myXmlFile.xml in root dir");
+            // Write first element  
+            textWriter.WriteStartElement("Student");
+            textWriter.WriteStartElement("r", "RECORD", "urn:record");
+            // Write next element  
+            textWriter.WriteStartElement("Name", "");
+            textWriter.WriteString("Student");
+            textWriter.WriteEndElement();
+            // Write one more element  
+            textWriter.WriteStartElement("Address", "");
+            textWriter.WriteString("Colony");
+            textWriter.WriteEndElement();
+            // WriteChars  
+            byte[] ch = new byte[3];
+            ch[0] = 11;
+            ch[1] = 40;
+            ch[2] = 32;
+            textWriter.WriteStartElement("ByteArray");
+            textWriter.WriteBase64(ch, 0, ch.Length);
+            textWriter.WriteEndElement();
+            // Ends the document.  
+            textWriter.WriteEndDocument();
+            // close writer  
+            textWriter.Close();
+
+
+
         }
+
     }
 }
